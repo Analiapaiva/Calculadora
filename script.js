3b495a81c1d2320a61749b10b698b0143c45f60c
Analia Paiva <144301753+Analiapaiva@users.noreply.github.com>
2023-11-28 23:03:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
-const MAN = document.getElementById('man');
-
-const CALCULAR = document.getElementById('calcular');
-const ERROR = document.getElementById('error');
-const FLU = document.getElementById('flu');
-const MAN = document.getElementById('man');
+const MANTENIMIENTO = document.getElementById('man'); // Corregido el nombre de la variable
 
 CALCULAR.addEventListener('click', () => {
-    const Peso = document.getElementById('peso').value
-    if (PESO > 0){
+    const PESO = parseFloat(document.getElementById('peso').value); // Convertido a número y corregido el nombre de la variable
+    
+    if (PESO > 0) {
         ERROR.style.display = 'none';
 
-        let flujo = Holliday (PESO);
-        MANTENIMIENTO.innerHTML= flujo /24
-        MANTENIENTO.style.display = 'block' 
-        MANTENIMIENTO2.innerHTML = (flujo/24) * 1.5
-        MANTENIMIENTO2.style.display = 'block'
-        FLUJO.innerHTML = flujo
-        FLUJO.style.display = 'block' 
-        console.log('Vol.diario =', Holliday(PESO));
-        console.log('Mantenimiento =',((Holliday(PESO)/24), 'cc/h')
-        console.log( 'Mantenimiento m+m/2 =', ((Holliday (PESO)/24)*1.5),'cc/h');
-    } else{
-        console.log('Superficie Corporal 1=',(superficie(PESO)*1500),'cc/h');
-        console.log('Superficie Corporal 2 =', (superficie(PESO)*2000), 'cc/h');
+        let flujo = calcularVolumenDiario(PESO); // Corregido el nombre de la función
+        MANTENIMIENTO.innerHTML = flujo.volumen1500 / 24; // Corregido el nombre de la propiedad
+        MANTENIMIENTO.style.display = 'block';
+        FLU.innerHTML = flujo.volumen1500; // Corregido el nombre de la propiedad
+        FLU.style.display = 'block';
+
+        console.log('Vol.diario =', flujo.volumen1500);
+        console.log('Mantenimiento =', flujo.volumen1500 / 24, 'cc/h'); // Corregido el paréntesis y la coma
+        console.log('Mantenimiento m+m/2 =', flujo.volumen1500 * 1.5 / 24, 'cc/h'); // Corregido el paréntesis y la coma
+    } else {
+        console.log('Superficie Corporal 1=', (calcularVolumenDiario(PESO).volumen1500), 'cc/h');
+        console.log('Superficie Corporal 2 =', (calcularVolumenDiario(PESO).volumen2000), 'cc/h');
     }
-    
+
     function calcularVolumenDiario(peso) {
         const superficieCorporal = ((peso * 4) + 7) / (peso + 90);
     
         const volumen1500 = superficieCorporal * 1500;
         const volumen2000 = superficieCorporal * 2000;
     
-        //  en este caso, devolvemos ambos resultados
         return {
             volumen1500: volumen1500,
             volumen2000: volumen2000
         };
     }
+});
+```
